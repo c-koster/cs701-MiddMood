9a72fc9519774e4f212888133ed26c07baa7966f
c-koster <52757828+c-koster@users.noreply.github.com>
2021-03-31 16:05:38 -0400

    much pushed

---
+++ experiment0.py
@@ -0,0 +1,135 @@
+from collections import Counter
+
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud, STOPWORDS
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+import operator
+
+from news_scraping_lib import
+
+stopwords = set(STOPWORDS)
+
+
+CSV_PATH = "data/first_scrape.csv"
+
+def user_tweet_skew():
+    counts = Counter()
+
+    with open(CSV_PATH) as fp:
+        csv_reader = csv.DictReader(fp)
+        for row in csv_reader:
+            u=row["username"]
+            counts[u] +=1
+
+    df = pd.DataFrame.from_dict(counts,orient='index',columns=['tweets'])
+    df['tweets'].hist(bins=100)
+    plt.show()
+    return
+
+
+def cloud():
+    tweets = pd.read_csv(CSV_PATH)
+    #tweets = tweets[tweets['username'] != "bhrenton"]
+    #tweets = tweets[tweets['username'] != ""]
+    print(tweets['tweet'])
+
+    wordcloud = WordCloud(
+        background_color='white',
+        stopwords=stopwords,
+        max_words=200,
+        max_font_size=40,
+        random_state=45
+    ).generate(str(tweets['tweet']))
+    fig = plt.figure(1)
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
+    return
+
+
+def get_corpus(data_src):
+    """
+    Sometimes you just want a text corpus of words all jumbled together so you can
+    make a bag of words.
+    """
+
+    corpus = ""
+    if data_src == "TWEETS":
+        with open(CSV_PATH) as fp:
+            csv_reader = csv.DictReader(fp)
+            for row in csv_reader:
+                u=row["tweet"]
+                corpus = corpus + " " + u
+        return corpus.replace(r'[^\s\w]','')
+    else:
+        return False
+
+
+
+def word_bag(corpus):
+
+    word_features = CountVectorizer(
+        strip_accents="unicode",
+        lowercase=True,
+        ngram_range=(1, 1),
+    )
+    text_to_words = word_features.build_analyzer()
+    assert(text_to_words("Hello hello world!") == ["hello", "hello", "world"])
+
+
+    words = Counter()
+
+    corpus_iter = text_to_words(corpus)
+    for word in corpus_iter:
+        if word not in STOPWORDS:
+            words[word] +=1
+
+    return words
+
+
+def get_word_index():
+    word_hash = {}
+    with open("data/dodds.txt",'r') as wordfile:
+            # Combine the lines in the list into a string
+            content = wordfile.readlines()
+            content = content[4:]
+            for word_info in content[:10]:
+                w = word_info.split()
+                word = w[0]
+                word_score = w[2]
+                word_hash[word] = word_score # add it to the dict
+            #content = "".join(content)
+
+    return word_hash
+
+
+
+def dodds_word_score(corpus,word_scores):
+    """
+    Perform a dodds-grading scheme on a corpus/bag of words. Ignore (but record)
+    middlebury-specific or twitter specific words which don't get spotted.
+    """
+    h_avg = 0
+    words_tot = 0
+    for w in corpus.items():
+        word = w[0]
+        freq = w[1]
+        if w[0] in word_scores.keys():
+
+            print(word,freq)
+
+
+
+
+
+if __name__ == "__main__":
+    # maybe useful to decide which experiment to run based on text input
+    word_hash = get_word_index()
+    c = get_corpus("NEWS")
+    tweets_bag = word_bag(c)
+    dodds_word_score(corpus=tweets_bag,word_scores=word_hash)
--- scraping-libraries/news_scraping_lib.py
+++ scraping-libraries/news_scraping_lib.py
@@ -1,23 +1,24 @@
+# so the functions here return headlines as headline/link pairs
+# I want a list of articles
+
+
+
+
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import tqdm
 
 # globals - noodle
 noodle_url = "https://thelocalnoodle.com/page/"
 
 # globals - campus
-begin = 35088
+begin = 54164
 
 campus_url = 'https://middleburycampus.com/'
 
 # the two sitemaps which include articles from our desired date range
 campus_categories = ['sports','news','local','opinion','arts-academics']
-
-@dataclass
-class Article:
-    date: str
-    headline: str
-    link: str
-    text: str
 
 def get_headlines_noodle(page):
     headlines = []
@@ -34,6 +35,7 @@ def get_headlines_noodle(page):
         headlines.append((e.text,link))
 
 
+
     return headlines
 
 def fetch_all_noodle():
@@ -44,6 +46,39 @@ def fetch_all_noodle():
         noodle_headlines += get_headlines_noodle(url_i)
 
     return noodle_headlines
+
+
+def get_text_noodle(article):
+    title = article[0]
+    link = article[1]
+    r = requests.get(link)
+    html_doc = r.content
+
+    return
+
+
+def get_text_campus(article):
+
+    # unpack the article tuple:
+    # title will be over-written because my title parser for campus articles is unreliable
+    link = article[1]
+
+    r = requests.get(link)
+    html_doc = r.content
+    soup = BeautifulSoup( html_doc , 'html.parser')
+
+
+    text = ""
+    content = soup.find_all("span",{"class": "storycontent"})[0]
+    for p in content.find_all("p"):
+        text = text + p.text
+
+    date_text = soup.find("span",{"class": "time-wrapper"}).text
+    date = datetime.strptime(date_text, '%B %d, %Y') # this is still a datetime object
+
+    title = soup.find("h1",{"class": "storyheadline"}).text
+
+    return {'title':title,'text':text,'date':date,'link':link}
 
 
 def fetch_articles_campus(maps):
@@ -65,8 +100,35 @@ def fetch_articles_campus(maps):
 
     return articles
 
+
 def fetch_all_campus():
     # now let's use the campus sitemap to get all of its headlines. the noodle started
     # in september of 2016, so let's start our scrape there.
-    maps = ['wp-sitemap-posts-post-7.xml','wp-sitemap-posts-post-8.xml']
+    maps = ['wp-sitemap-posts-post-8.xml']
     return fetch_articles_campus(maps)
+
+
+
+
+if __name__ == "__main__":
+    campus = fetch_all_campus()
+    #noodle = fetch_all_noodle()
+
+    campus_texts_full = []
+    for article_pair in campus:
+        campus_texts_full.append(get_text_campus(article_pair))
+        #print(article_pair[0])
+
+
+    seven_days_ago = datetime.now() - timedelta(days=7)
+    for a in campus_texts_full:
+        if a['date'] > seven_days_ago:
+            a['date'] = str(a['date'])
+            print(a)
+
+    """
+    noodle_texts_full = []
+    for article_pair in noodle:
+        noodle_texts_full.append(get_text_noodle(article_pair))
+    """
+    # eh just filter by date and dump it all to stdout ... ?
